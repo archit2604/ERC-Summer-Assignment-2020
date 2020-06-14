8892f374fcf97990107ab36f65f41f7b7e1b3df7
ARCHIT RUNGTA <54287135+archit2604@users.noreply.github.com>
2020-06-14 18:53:41 +0530

    Update path_planner.py

--- omnibase_controller/src/path_planner.py
+++ omnibase_controller/src/path_planner.py
@@ -5,8 +5,10 @@ from geometry_msgs.msg import PoseStamped
 rospy.init_node('path_planner_omni')
 pub=rospy.Publisher('path',Path,queue_size=1)
 rate=rospy.Rate(10)
+#decrales a path object
 path=Path()
 path.header.frame_id="path"
+#Takes the path as input from the user
 while True:
     x=float(raw_input("Enter the x coordinate of the point:"))
     y=float(raw_input("Enter the y coordinate of the point:"))
