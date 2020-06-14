e4765dec64c367e5c3d6d8fb0d788e72ff3a1aae
ARCHIT RUNGTA <54287135+archit2604@users.noreply.github.com>
2020-06-14 17:49:01 +0530

    Add files via upload

---
+++ omnibase_controller/src/controller.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Path
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+def callback_path(msg):
+    global x_g
+    global y_g
+    global flag
+    global l
+    l=len(msg.poses)
+    x_g=msg.poses[flag].pose.position.x
+    y_g=msg.poses[flag].pose.position.y
+def callback_odom(msg):
+    global l
+    global x_g
+    global y_g
+    global e_x_sum
+    global e_x_prev
+    global e_y_sum
+    global e_y_prev
+    global count1
+    global count2
+    global flag
+    twist=Twist()
+    x=msg.pose.pose.position.x
+    y=msg.pose.pose.position.y
+    if count1==0:
+        e_x=x_g-x
+        e_x_sum+=e_x
+        dedt_x=e_x-e_x_prev
+        u_x=0.50*e_x+0.0008*e_x_sum+0.17*dedt_x
+        twist.linear.x=u_x
+        pub.publish(twist)
+        e_x_prev=e_x
+        if e_x<=0.01 and e_x>=-0.01:
+            print(e_x)
+            twist.linear.x=0
+            pub.publish(twist)
+            count1=1
+    if count2==0:   
+        e_y=y_g-y
+        e_y_sum+=e_y
+        dedt_y=e_y-e_y_prev
+        u_y=0.50*e_y+0.0008*e_y_sum+0.17*dedt_y
+        twist.linear.y=u_y
+        pub.publish(twist)
+        e_y_prev=e_y 
+        if e_y<=0.01 and e_y>=-0.01:
+            print(e_y)
+            twist.linear.y=0
+            pub.publish(twist)
+            count2=1
+    if count1==1 and count2==1:
+        if flag+1<l:
+            count1=0
+            count2=0
+            flag+=1
+            rospy.sleep(1)
+global count1
+count1=0
+global count2
+count2=0
+global e_x_sum
+e_x_sum=0
+global e_x_prev
+e_x_prev=0
+global e_y_sum
+e_y_sum=0
+global e_y_prev
+e_y_prev=0
+global flag
+flag=0
+rospy.init_node('controller_omni')
+rospy.sleep(1)
+sub_path=rospy.Subscriber('path',Path,callback_path)
+sub_odom=rospy.Subscriber('odom',Odometry,callback_odom)
+pub=rospy.Publisher('cmd_vel',Twist,queue_size=2)
+rospy.spin()
