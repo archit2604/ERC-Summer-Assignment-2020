28ab0b003caf8f5c5077e1403d44176f9f5de9a3
ARCHIT RUNGTA <54287135+archit2604@users.noreply.github.com>
2020-06-14 18:47:26 +0530

    Update follower.py

--- follower/src/follower.py
+++ follower/src/follower.py
@@ -2,9 +2,11 @@
 import rospy, cv2, cv_bridge, numpy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
+#Defines PI
 PI = 3.1415926535897
 count=0
 class Follower:
+    #Constructor for the class
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
         cv2.namedWindow("window", 1)
@@ -14,22 +16,31 @@ class Follower:
         self.e_prev=0
         self.e_sum=0
         self.e=0
+    #function to process image and control the turtlebot accordingly
     def image_callback(self, msg):
+        #image conversion from ROS to OpenCV format
         image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+        #image conversion from RGB to HSv format
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        #describes the range of yellow color in HSV format
         lower_yellow = numpy.array([ 10, 10, 10])
         upper_yellow = numpy.array([255, 255, 250])
+        #masks image to detect yellow color
         mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
         h, w, d = image.shape
         search_top = 3*h/4-10
         search_bot = 3*h/4+10
+        #makes all pixels except the required region to 0
+        #required region is the are just in front of the tb3
         mask[0:search_top, 0:w] = 0
         mask[search_bot:h, 0:w] = 0
         if mask.any():
+            #Defines the centre of the required region
             M = cv2.moments(mask)
             if M['m00'] > 0:
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
+                #Implementing a PID controller
                 self.e = cx - w/2
                 self.e_sum+=self.e
                 dedt=self.e-self.e_prev
@@ -43,5 +54,6 @@ class Follower:
         cv2.imshow("window", image)
         cv2.waitKey(3)
 rospy.init_node('follower')
+#Makes a object named follower of the class Follower
 follower = Follower()
 rospy.spin()
