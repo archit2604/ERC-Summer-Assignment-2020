20062d5ed7010da1704da9a9bbfef92c0e5c9203
ARCHIT RUNGTA <54287135+archit2604@users.noreply.github.com>
2020-06-14 18:37:19 +0530

    Add files via upload

---
+++ follower/src/follower.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import rospy, cv2, cv_bridge, numpy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+PI = 3.1415926535897
+count=0
+class Follower:
+    def __init__(self):
+        self.bridge = cv_bridge.CvBridge()
+        cv2.namedWindow("window", 1)
+        self.image_sub = rospy.Subscriber('camera/rgb/image_raw',Image, self.image_callback)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel',Twist, queue_size=1)
+        self.twist = Twist()
+        self.e_prev=0
+        self.e_sum=0
+        self.e=0
+    def image_callback(self, msg):
+        image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower_yellow = numpy.array([ 10, 10, 10])
+        upper_yellow = numpy.array([255, 255, 250])
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        h, w, d = image.shape
+        search_top = 3*h/4-10
+        search_bot = 3*h/4+10
+        mask[0:search_top, 0:w] = 0
+        mask[search_bot:h, 0:w] = 0
+        if mask.any():
+            M = cv2.moments(mask)
+            if M['m00'] > 0:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                self.e = cx - w/2
+                self.e_sum+=self.e
+                dedt=self.e-self.e_prev
+                self.twist.linear.x = 0.2
+                self.twist.angular.z =-float(0.01*self.e+0.000005*self.e_sum+0.005*dedt)/15
+                self.cmd_vel_pub.publish(self.twist)
+        else:
+            self.twist.linear.x = 0
+            self.twist.angular.z = 0
+            self.cmd_vel_pub.publish(self.twist)
+        cv2.imshow("window", image)
+        cv2.waitKey(3)
+rospy.init_node('follower')
+follower = Follower()
+rospy.spin()
